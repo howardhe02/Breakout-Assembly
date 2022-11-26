3fb59bf23c7aaa41ec7eaf49748ab6d265aee7da
john <jfitzgerald1126@gmail.com>
2022-11-26 00:01:39 -0500

    draw bricks + border

--- breakout.asm
+++ breakout.asm
@@ -2,7 +2,7 @@
 # This file contains our implementation of Breakout.
 #
 # Student 1: Name, Student Number
-# Student 2: John Fitzgerald, 1008155513
+# Student 2: Name, Student Number
 ######################## Bitmap Display Configuration ########################
 # - Unit width in pixels:       8
 # - Unit height in pixels:      8
@@ -15,16 +15,15 @@
 ##############################################################################
 # Immutable Data
 COLOURS: 
-	.word 0xe6261f	#red (0)
-	.word 0xeb7532	#orange (4)
-	.word 0xf7d038	#yellow (8)
-	.word 0xa3e048	#green (12)
-	.word 0x49da9a	#jade (16)
-	.word 0x34bbe6	#sky (20)
-	.word 0x4355db	#blue (24)
-	.word 0xd23be7 #purple (28)
-	.word 0x000000	#black (32)
-	.word 0xfffdd0	#lean (36)
+	.word 0xe6261f	#red
+	.word 0xeb7532	#orange
+	.word 0xf7d038	#yellow
+	.word 0xa3e048	#green
+	.word 0x49da9a	#jade
+	.word 0x34bbe6	#sky
+	.word 0x4355db	#blue
+	.word 0x000000	#black
+	.word fffdd0	#lean
 	
 ##############################################################################
 # The address of the bitmap display. Don't forget to connect it!
@@ -51,21 +50,17 @@ BALL:
 	# Run the Brick Breaker game.
 main:
     # Initialize the game
-    jal draw_walls
-
-
-
 
 # get_location_address(x, y) -> address
 #   Return the address of the unit on the display at location (x,y)
 #
 #   Preconditions:
-#       - x is between 0 and 64, inclusive
-#       - y is between 0 and 64, inclusive
+#       - x is between 0 and 31, inclusive
+#       - y is between 0 and 31, inclusive
 get_location_address:
 	# BODY
 	sll $a0, $a0, 2 # x_bytes = x * 4
-	sll $a1, $a1, 8 # y_bytes = y * 256
+	sll $a1, $a1, 7 # y_bytes = y * 128
 	
 	la $v0, ADDR_DSPL
 	lw $v0, 0($v0)
@@ -106,51 +101,8 @@ draw_line_epi:
 #   Draw the walls of the breakout game on the display
 #
 draw_walls:
-# draw left wall 
-
-
-# draw top wall (4 lines from x = 4 -> x = 59) 
-	li $a0, 4 # x_value
-	li $a1, 0 # y_value
-	jal get_location_address # returns loc_addrsss in $v0
-	add $t0, $v0, $zero # store starting location for first row
-	
-	addi $a1, $a1, 256 # add 1 row to y_value
-	jal get_location_address # returns loc_addrsss in $v0
-	add $t1, $v0, $zero # store starting location for second row
-	
-	addi $a1, $a1, 256 # add 1 row to y_value
-	jal get_location_address # returns loc_addrsss in $v0
-	add $t2, $v0, $zero # store starting location for third row
-	
-	addi $a1, $a1, 256 # add 1 row to y_value 
-	jal get_location_address # returns loc_addrsss in $v0
-	add $t3, $v0, $zero # store starting location for fourth row
-	
-	
-	add $a0, $t0, $zero # prepare to call draw_line with start position (0, 4)
-	la $a1, COLOURS # colour
-	addi $a1, $a1, 36
-	li $a2, 55 # draw 56 units across (units 4 - 60)
-	jal draw_line # draw first row
-	# TODO: NEED TO SAVE TEMP VALUES FROM ABOVE IN MEMORY because draw_line overwrites $t0, $t1 ...
-	add $a0, $t2, $zero
-	jal draw_line # draw second row
-	add $a0, $t3, $zero
-	jal draw_line # draw third row
-	add $a0, $t4, $zero
-	jal draw_line # draw fourth row
-	
-
-
-# draw right wall
-	
 	
 draw_walls_loop:
-
-
-
-
 
 game_loop:
 	# 1a. Check if key has been pressed
